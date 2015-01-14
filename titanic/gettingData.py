a4a02ad1703edaa74e2949e14a66bba6c073ec6e
kunal arora <kunalarora.135@gmail.com>
2015-01-14 20:35:26 +0530

    gender based model

---
+++ titanic/gettingData.py
@@ -0,0 +1,11 @@
+import csv as csv
+import numpy as np
+
+csv_file_object=csv.reader(open('data/train.csv','rb'))
+header = csv_file_object.next()
+
+data=[]
+for row in csv_file_object:
+	data.append(row)
+data=np.array(data)
+print data[0::,1]
