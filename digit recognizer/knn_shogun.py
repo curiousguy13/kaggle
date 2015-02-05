5b13d4ba0ac8bdb7eac34ffb27638b5ad4fa9fa0
kunal arora <kunalarora.135@gmail.com>
2015-02-05 23:36:50 +0530

    knn_shogun seems to be working now

--- digit recognizer/knn_shogun.py
+++ digit recognizer/knn_shogun.py
@@ -2,7 +2,7 @@
 import numpy as np
 import pandas as pd
 import csv as csv
-from sklearn.neighbors import KNeighborsClassifier
+#from sklearn.neighbors import KNeighborsClassifier
 from modshogun import MulticlassLabels, RealFeatures
 from modshogun import KNN, EuclideanDistance
 
@@ -15,11 +15,16 @@ print 'Training...'
 
 #converting train data to numpy array
 
+
 train_data=train_db.values
-x_train=train_data[0::,1::]
-y_train=train_data[0::,0]
+#mat  = loadmat('../../../data/multiclass/usps.mat')
+x_train=np.array(train_data[0::,1::],dtype=np.double)
+#squeeze function is used to convert a matrix to array and MultiClassLabels requires a vector,not a matrix
+y_train=np.array(train_data[0::,0].squeeze(),dtype=np.double)
+
+#y_train=train_data[0::,0]
 labels=MulticlassLabels(y_train)
-feats=RealFeatures(x_train)
+feats=RealFeatures(x_train.T)
 k=3
 
 dist=EuclideanDistance()
@@ -31,17 +36,28 @@ print 'Predicting...'
 
 #converting train data to numpy array
 test_data=test_db.values
-x_test=test_data[0::,1::]
-y_test=test_data[0::,0]
 
-output = knn.apply_multiclass(test_data).astype(int)
+x_test=np.array(test_data[0::,0::],dtype=np.double)
 
 
+feats_test=RealFeatures(x_test.T)
+output = knn.apply_multiclass(feats_test)
+
+
+count=x_test.shape[0]
+ids=[]
+for i in range(1,count+1):
+	ids.append(i)
+int_out=[]
+
+for i in range(0,count):
+	int_out.append(int(output[i]))
+	
 #put the output in a file
 predictions_file = open("knn_shogun.csv", "wb")
 open_file_object = csv.writer(predictions_file)
 open_file_object.writerow(["ImageId","Label"])
-open_file_object.writerows(zip(ids,output))
+open_file_object.writerows(zip(ids,int_out))
 predictions_file.close()
 print 'Done.'
 
