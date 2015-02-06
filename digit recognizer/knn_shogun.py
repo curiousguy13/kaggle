510cc42496f3f8b9383d5a4e0c19021f25f407d8
kunal arora <kunalarora.135@gmail.com>
2015-02-06 23:47:53 +0530

    cleanup and documentation in knn_shogun.py

--- digit recognizer/knn_shogun.py
+++ digit recognizer/knn_shogun.py
@@ -2,7 +2,6 @@
 import numpy as np
 import pandas as pd
 import csv as csv
-#from sklearn.neighbors import KNeighborsClassifier
 from modshogun import MulticlassLabels, RealFeatures
 from modshogun import KNN, EuclideanDistance
 
@@ -14,19 +13,18 @@ test_db=pd.read_csv("test.csv",header=0)
 print 'Training...'
 
 #converting train data to numpy array
-
-
 train_data=train_db.values
 #mat  = loadmat('../../../data/multiclass/usps.mat')
 x_train=np.array(train_data[0::,1::],dtype=np.double)
 #squeeze function is used to convert a matrix to array and MultiClassLabels requires a vector,not a matrix
 y_train=np.array(train_data[0::,0].squeeze(),dtype=np.double)
 
-#y_train=train_data[0::,0]
+#required for shogun's algorithm
 labels=MulticlassLabels(y_train)
 feats=RealFeatures(x_train.T)
 k=3
 
+#training the data using knn algorithm
 dist=EuclideanDistance()
 knn=KNN(k,dist,labels)
 knn.train(feats)
@@ -34,22 +32,25 @@ knn.train(feats)
 
 print 'Predicting...'
 
-#converting train data to numpy array
+#converting test data to numpy array
 test_data=test_db.values
-
+#converting x_test to numpy array
 x_test=np.array(test_data[0::,0::],dtype=np.double)
 
-
+#applysing the knn shogun algorithm on to predict values in the test file
 feats_test=RealFeatures(x_test.T)
 output = knn.apply_multiclass(feats_test)
 
-
+#size of input
 count=x_test.shape[0]
+
+#creating the ids array
 ids=[]
 for i in range(1,count+1):
 	ids.append(i)
-int_out=[]
 
+#getting the integer values of the output
+int_out=[]
 for i in range(0,count):
 	int_out.append(int(output[i]))
 	
