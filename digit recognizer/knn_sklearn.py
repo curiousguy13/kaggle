18555995902726e927d215e96c93a7d2467beae0
kunal arora <kunalarora.135@gmail.com>
2015-02-09 23:53:21 +0530

    renamed knn_sklearn.py

---
+++ digit recognizer/knn_sklearn.py
@@ -0,0 +1,42 @@
+#digitRecognizer using k-nearest neighbour algorithm fom scikit-learn
+import numpy as np
+import pandas as pd
+import csv as csv
+from sklearn.neighbors import KNeighborsClassifier
+
+#header=0 makes zeroth row in csv as coulumn names
+train_db=pd.read_csv("train.csv",header=0)
+test_db=pd.read_csv("test.csv",header=0)
+
+#use k-nn of scikit-learn to train this data
+print 'Training...'
+
+#converting train data to numpy array
+train_data=train_db.values
+neigh=KNeighborsClassifier(n_neighbors=3)
+
+#passing the data to the classifier
+neigh=neigh.fit(train_data[0::,1::],train_data[0::,0])
+
+print 'Predicting...'
+
+#converting train data to numpy array
+test_data=test_db.values
+
+output = neigh.predict(test_data).astype(int)
+
+count=len(output)
+ids=[]
+for i in range(1,count):
+	ids.append(i)
+
+#put the output in a file
+predictions_file = open("knn_sklearn.csv", "wb")
+open_file_object = csv.writer(predictions_file)
+open_file_object.writerow(["ImageId","Label"])
+open_file_object.writerows(zip(ids,output))
+predictions_file.close()
+print 'Done.'
+
+
+
